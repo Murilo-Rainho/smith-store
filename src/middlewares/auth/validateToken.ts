e692d4af800de3535bb595d95896fb8a7a9dc33a
Murilo Rainho <murilorainho01@gmail.com>
2022-02-22 22:31:15 -0300

    [feat]: create a validateToken middleware

--- src/middlewares/auth/validateToken.ts
+++ src/middlewares/auth/validateToken.ts
@@ -1,3 +1,29 @@
-const validateToken = () => {};
+import { NextFunction, Response, Request } from 'express';
+
+import jwt from 'jsonwebtoken';
+
+const validateToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const token = req.headers.authorization;
+  
+    if (!token) {
+      return res.status(401).json({ error: 'Token not found' });
+    }
+  
+    // this usually stays in the environment variables.
+    // Here, it's only out, for study reasons.
+    const secret = 'mySuperSecret';
+  
+    jwt.verify(token, secret);
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
 
 export default validateToken;
